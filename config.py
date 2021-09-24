0637c9776a263362c18c623ad35cbe1bccf07c6c
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-09-24 23:56:48 +0300

    add root files and edited config files to configure database and url.

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+
+class Config:
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    #email configurations
+    MAIL_SERVER = 'smpt.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL','')
+    if SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://',1)
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://patrick:patrick@localhost/blogs'
+
+config_options = {
+    'developmen':DevConfig,
+    'production':ProdConfig
+}
