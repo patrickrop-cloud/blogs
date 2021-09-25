71d1e14ed6597e3f7d5c9e972a4633b64ace18af
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-09-25 12:54:02 +0300

    add profile folder to templates and add files to execute profile functionality

--- manage.py
+++ manage.py
@@ -2,8 +2,8 @@ from app import create_app
 from flask_script import Manager,Server
 
 
-
-app = create_app('production')
+app = create_app('development')
+# app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('server',Server)
