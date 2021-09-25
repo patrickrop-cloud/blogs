65b248fb224854e4180d808e6a54b0ded9640cb8
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-09-25 11:51:01 +0300

    add blog and comment model functionality to save the blogs and comments posted on database.

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from enum import unique
+import re
 
 from sqlalchemy.orm import backref
 from . import login_manager
@@ -48,4 +49,30 @@ class User(UserMixin,db.Model):
 class Blog(db.Model):
     __tablename__ = 'blogs'
 
-    id = db.Column
+    id = db.Column(db.Integer, primary_key = True)
+    tittle = db.Column(db.String(80))
+    content = db.Column(db.String())
+    created_at = db.Column(db.DateTime,default = datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comments = db.relationship('Comment', backref = 'blog', lazy = "dynamic")
+
+    def save_blog(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_user_blogs(user_id):
+        return Blog.query.filter_by(user_id = user_id).all()
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String())
+    blog_id = db.Column(db.Integer, db.ForeignKey('blogs.id'))
+    user_id = db.Columm(db.Integer, db.ForeignKey('users.id'))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        return self.id
