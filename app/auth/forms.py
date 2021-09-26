039e2a6d403897a0a99e9a85e72dd497c44b9d9a
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-09-27 00:12:55 +0300

    debug forms and views files to allow user to sign up and create a blog post.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,33 +1,29 @@
 from flask_wtf import FlaskForm
-from wtforms import SelectField,SubmitField,PasswordField
+# from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import Required,Email,EqualTo
+from ..models import User
 from wtforms import ValidationError
-# from ..models import User
-from wtforms import StringField,BooleanField,PasswordField,SubmitField
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
+
 
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
-    username = StringField('Enter your username',validators=[Required()])
-    password = PasswordField('Password',validators=[Required(), EqualTo('password_cornfirm',message='Passwords must match')])
-    password_confirm = PasswordField('Confirm Passwords',validators=[Required()])
+    username = StringField('Enter your username',validators = [Required()])
+    password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
     submit = SubmitField('Sign Up')
 
-    def validate_email(self, data_field):
-        if User.query.filter_by(email = data_field.data).first():
-            raise ValidationError('There is an existing account with this email')
 
-    def validate(self,data_field):
+    def validate_email(self,data_field):
+            if User.query.filter_by(email =data_field.data).first():
+                raise ValidationError('There is an account with that email')
+
+    def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
-            raise ValidationError('The user name entered is already taken')
-
+            raise ValidationError('That username is taken')
+        
 class LoginForm(FlaskForm):
-    email = StringField('Enter your Email Adress',validators=[Required(),Email()])
-    password = PasswordField('Password',validators=[Required()])
-    remember = BooleanField('Remember Me') 
-    submit = SubmitField('Sign In')       
-
-
-
-
-
-
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,11 @@
-from flask import render_template,redirect,url_for,flash,request
+from . import auth
 from flask_login import login_user,logout_user,login_required
+from .. import db
+from flask import render_template,redirect,url_for, flash,request
+from flask_login import login_user
 from ..models import User
 from .forms import LoginForm,RegistrationForm
 from ..email import mail_message
-from . import auth
-from .. import db
-from app import email
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -15,22 +15,27 @@ def login():
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user,login_form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
+
         flash('Invalid username or Password')
-    title = 'blog login'
+
+    title = "pitches login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
-@auth.route('/register',methods=['GET','POST'])
+@auth.route('/register',methods = ["GET","POST"])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email = form.email.data,username = form.username.data,password = form.password.data)
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
 
-        mail_message('Welcome to this blogpost site','email/welcome_user',user.email,user=user)
+        mail_message("Welcome to blogs","email/welcome_user",user.email,user=user)
+
         return redirect(url_for('auth.login'))
-        title = 'New Account'
-    return render_template('auth/register.html',registration_form=form)
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
+
+
 
 @auth.route('/logout')
 @login_required
