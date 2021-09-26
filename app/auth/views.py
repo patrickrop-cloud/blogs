ba03b6ed2b78d8d937a7aa51012fc5cc0051e633
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-09-27 02:32:41 +0300

    add styling to the pages

--- app/auth/views.py
+++ app/auth/views.py
@@ -18,7 +18,7 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "pitches login"
+    title = "blogs login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
 @auth.route('/register',methods = ["GET","POST"])
